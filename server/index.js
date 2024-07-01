9742ab59596c19e91e36437a6f7f0c7ebc5ad1d2
Om Patel <omupatel7113@gmail.com>
2024-07-01 23:32:48 +0530

    Project setup

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
