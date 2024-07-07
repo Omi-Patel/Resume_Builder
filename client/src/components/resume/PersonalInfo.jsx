830a57f3927b502fec1d45bdfb5afcb0e49e47ec
Om Patel <omupatel7113@gmail.com>
2024-07-07 16:07:57 +0530

    education form done

--- client/src/components/resume/PersonalInfo.jsx
+++ client/src/components/resume/PersonalInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PersonalInfo = () => {
   return (
-    <div className="bg-blue-400 h-full">
+    <div className=" h-full">
       <h1 className="bg-red-100 text-3xl text-center font-bold font-mono tracking-tighter">
         Personal INFO.
       </h1>
@@ -10,14 +10,14 @@ const PersonalInfo = () => {
       {/* Input Fields */}
       <div className=" mt-8 max-w-4xl mx-auto">
         {/* First Row - Name & Email */}
-        <div className="grid grid-cols-2 gap-16 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-16 mt-4">
           <div>
             {/* <label htmlFor="name">Name</label> */}
             <input
               type="text"
               id="name"
               name="name"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="Your Name"
             />
           </div>
@@ -28,7 +28,7 @@ const PersonalInfo = () => {
               type="email"
               id="email"
               name="email"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="Your Email"
             />
           </div>
@@ -36,14 +36,14 @@ const PersonalInfo = () => {
 
         {/* Second Row - Mobile & Adderess */}
 
-        <div className="grid grid-cols-2 gap-16 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-16 mt-2 sm:mt-4">
           <div>
             {/* <label htmlFor="name">Name</label> */}
             <input
               type="tel"
               id="mobile"
               name="mobile"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="Your Mobile No."
             />
           </div>
@@ -54,21 +54,21 @@ const PersonalInfo = () => {
               type="text"
               id="address"
               name="address"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="Your Address"
             />
           </div>
         </div>
 
         {/* Third Row - Social Links */}
-        <div className="grid grid-cols-2 gap-16 mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-16 mt-2 sm:mt-4">
           <div>
             {/* <label htmlFor="name">Name</label> */}
             <input
               type="text"
               id="linkedin"
               name="linkedin"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="LinkedIn URL"
             />
           </div>
@@ -79,7 +79,7 @@ const PersonalInfo = () => {
               type="text"
               id="github"
               name="github"
-              className="block w-full px-4 py-2 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="block w-full px-4 py-3 mt-2 text-base text-black placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
               placeholder="GitHub URL"
             />
           </div>
@@ -98,6 +98,51 @@ const PersonalInfo = () => {
               placeholder="Summary"
             />
           </div>
+        </div>
+
+
+        {/* Buttons */}
+
+        <div className="bg-blue-500 mt-14 text-xl font-semibold tracking-wide flex justify-between items-center">
+          <button className="bg-yellow-300 px-8 py-3 rounded-lg">
+            <div className="flex gap-1 items-center justify-center">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              <span>Back</span>
+            </div>
+          </button>
+          <button className="bg-yellow-300 px-6 py-3 rounded-lg">
+            <div className="flex gap-1 items-center justify-center">
+              <span>Save & Next</span>
+
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </div>
+          </button>
         </div>
       </div>
     </div>
